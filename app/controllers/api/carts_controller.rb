34d41eb9a68553d4e8dc31382692bb241c564d87
raul <raul@appacademy.io>
2015-05-01 18:34:30 -0400

    Add to Cart adds correctly to cart

--- app/controllers/api/carts_controller.rb
+++ app/controllers/api/carts_controller.rb
@@ -2,20 +2,8 @@ class Api::CartsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
-
-    if current_user
-      @cart = Cart.find_by(buyer_id: current_user.id)
-    else
-      @cart = Cart.find_by(session_token: session[:guest_token])
-    end
-
-    p "before"
-    p @cart.products
-
-    @cart.products << @product
-
-    p "after"
-    p @cart.products
+    @cart = current_cart
+    @cart.products << @product unless @cart.products.include?(@product)
 
     redirect_to root_url
   end
