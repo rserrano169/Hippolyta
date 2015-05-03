b4b5177f0006cf89ee6aa843ae2e97da04da8bf2
raul <raul@appacademy.io>
2015-05-03 11:26:03 -0400

    change hippolyta logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ u1 = User.create({
   password: "hippolyta",
 })
 
-u1.picture = File.open(File.join(Rails.root, "/app/assets/images/seed/hippolyta_logo.png"))
+u1.picture = File.open(File.join(Rails.root, "/app/assets/images/seed/hippolyta_logo_white.png"))
 u1.save!
 
 p1 = Product.create({
