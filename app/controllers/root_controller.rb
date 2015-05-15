35629b7bf4ce7f44e545b20c4a0c82fe6235c590
raul <raul@appacademy.io>
2015-05-14 20:02:41 -0400

    persist guest cart_products attributes after sign in

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -10,8 +10,21 @@ class RootController < ApplicationController
     else
       @cart = Cart.find_by(buyer_id: current_user.id)
       @guest_cart = Cart.find_by(session_token: session[:guest_token])
+
       if @guest_cart
         @cart.products += @guest_cart.products
+        @guest_cart.cart_products.each do |g_cart_prod|
+
+          overlap = @cart.cart_products.find_by({
+            product_id: g_cart_prod.product_id
+          })
+          if overlap
+            overlap.update_attributes({
+              quantity: g_cart_prod.quantity
+            })
+          end
+        end
+
         @guest_cart.destroy
       end
     end
