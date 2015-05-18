e06d695de4b885c18b50f357a023ecd77ced6231
raul <raul@appacademy.io>
2015-05-18 16:45:16 -0400

    persist search field input

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :current_user, :signed_in?, :current_cart, :money_string
+  helper_method :current_user,
+                :signed_in?,
+                :current_cart,
+                :money_string,
+                :last_query
 
   private
 
@@ -24,12 +28,22 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def last_query
+    if current_user
+      current_user.queries.last
+    else
+      Query.where(session_token: session[:guest_token]).last
+    end
+  end
+
   def signed_in?
     !!current_user
   end
 
   def sign_in(user)
+    @last_query = last_query
     session[:user_token] = user.reset_session_token!
+    current_user.queries << @last_query unless @last_query.nil?
   end
 
   def sign_out
