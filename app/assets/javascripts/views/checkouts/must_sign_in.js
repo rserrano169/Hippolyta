d3328ce062a53480d8bcd979c3ac0797aed2dbfd
raul <raul@appacademy.io>
2015-05-27 01:07:27 -0400

    style sign-in and sign-up pages, and change cart-dropdown to have checkout button

--- app/assets/javascripts/views/checkouts/must_sign_in.js
+++ app/assets/javascripts/views/checkouts/must_sign_in.js
@@ -3,9 +3,11 @@ Hippolyta.Views.SignInBeforeCheckout = Backbone.View.extend({
   template: JST["checkouts/must_sign_in"],
 
   initialize: function () {
+  },
+
+  render: function () {
     var content = this.template({});
     this.$el.html(content);
     return this;
   },
-
 });
