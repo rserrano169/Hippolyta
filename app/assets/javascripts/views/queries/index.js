4f784b880a01337246e69f508c6954652ee13d6c
raul <raul@appacademy.io>
2015-07-13 17:47:24 -0400

    render search results review stars by percentage in queries index view

--- app/assets/javascripts/views/queries/index.js
+++ app/assets/javascripts/views/queries/index.js
@@ -7,8 +7,22 @@ Hippolyta.Views.SearchResults = Backbone.View.extend({
   },
 
   render: function () {
-    var content = this.template({ results: this.collection });
+    var content = this.template({
+      results: this.collection,
+      renderStarRating: this.renderStarRating,
+    });
     this.$el.html(content);
+    this.renderStarRating();
+
     return this;
+  },
+
+  renderStarRating: function () {
+    this
+      .$el
+      .find($("input[name='reviewRatingPercentage']"))
+      .each( function (idx) {
+        $(this).parent().width($(this).val());
+      });
   },
 });
